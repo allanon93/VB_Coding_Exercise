d463ed59d0d56f1ba28131d9431de0f32b5de33f
allanon93 <cyrisxzeiders@gmail.com>
2023-05-13 12:22:10 -0400

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblCodeTitle = New Label()
+        btnExercise1 = New Button()
+        btnExercise2 = New Button()
+        btnExercise3 = New Button()
+        btnExercise4 = New Button()
+        btnExercise5 = New Button()
+        btnExercise6 = New Button()
+        btnExercise7 = New Button()
+        btnExercise8 = New Button()
+        lblExercise1 = New Label()
+        lblExercise2 = New Label()
+        lblExercise3 = New Label()
+        lblExercise4 = New Label()
+        lblExercise5 = New Label()
+        lblExercise6 = New Label()
+        lblExercise7 = New Label()
+        lblExercise8 = New Label()
+        SuspendLayout()
+        ' 
+        ' lblCodeTitle
+        ' 
+        lblCodeTitle.AutoSize = True
+        lblCodeTitle.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        lblCodeTitle.Location = New Point(229, 9)
+        lblCodeTitle.Name = "lblCodeTitle"
+        lblCodeTitle.Size = New Size(195, 32)
+        lblCodeTitle.TabIndex = 0
+        lblCodeTitle.Text = "Coding Exercise"
+        ' 
+        ' btnExercise1
+        ' 
+        btnExercise1.Location = New Point(34, 50)
+        btnExercise1.Name = "btnExercise1"
+        btnExercise1.Size = New Size(75, 23)
+        btnExercise1.TabIndex = 1
+        btnExercise1.Text = "Exercise 1"
+        btnExercise1.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise2
+        ' 
+        btnExercise2.Location = New Point(34, 79)
+        btnExercise2.Name = "btnExercise2"
+        btnExercise2.Size = New Size(75, 23)
+        btnExercise2.TabIndex = 2
+        btnExercise2.Text = "Exercise 2"
+        btnExercise2.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise3
+        ' 
+        btnExercise3.Location = New Point(34, 108)
+        btnExercise3.Name = "btnExercise3"
+        btnExercise3.Size = New Size(75, 23)
+        btnExercise3.TabIndex = 3
+        btnExercise3.Text = "Exercise 3"
+        btnExercise3.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise4
+        ' 
+        btnExercise4.Location = New Point(34, 137)
+        btnExercise4.Name = "btnExercise4"
+        btnExercise4.Size = New Size(75, 23)
+        btnExercise4.TabIndex = 4
+        btnExercise4.Text = "Exercise 4"
+        btnExercise4.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise5
+        ' 
+        btnExercise5.Location = New Point(34, 166)
+        btnExercise5.Name = "btnExercise5"
+        btnExercise5.Size = New Size(75, 23)
+        btnExercise5.TabIndex = 5
+        btnExercise5.Text = "Exercise 5"
+        btnExercise5.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise6
+        ' 
+        btnExercise6.Location = New Point(34, 195)
+        btnExercise6.Name = "btnExercise6"
+        btnExercise6.Size = New Size(75, 23)
+        btnExercise6.TabIndex = 6
+        btnExercise6.Text = "Exercise 6"
+        btnExercise6.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise7
+        ' 
+        btnExercise7.Location = New Point(34, 224)
+        btnExercise7.Name = "btnExercise7"
+        btnExercise7.Size = New Size(75, 23)
+        btnExercise7.TabIndex = 7
+        btnExercise7.Text = "Exercise 7"
+        btnExercise7.UseVisualStyleBackColor = True
+        ' 
+        ' btnExercise8
+        ' 
+        btnExercise8.Location = New Point(34, 253)
+        btnExercise8.Name = "btnExercise8"
+        btnExercise8.Size = New Size(75, 23)
+        btnExercise8.TabIndex = 8
+        btnExercise8.Text = "Exercise 8"
+        btnExercise8.UseVisualStyleBackColor = True
+        ' 
+        ' lblExercise1
+        ' 
+        lblExercise1.AutoSize = True
+        lblExercise1.Location = New Point(115, 54)
+        lblExercise1.Name = "lblExercise1"
+        lblExercise1.Size = New Size(335, 15)
+        lblExercise1.TabIndex = 9
+        lblExercise1.Text = "Output each item in a seperate message box one after another"
+        ' 
+        ' lblExercise2
+        ' 
+        lblExercise2.AutoSize = True
+        lblExercise2.Location = New Point(115, 83)
+        lblExercise2.Name = "lblExercise2"
+        lblExercise2.Size = New Size(353, 15)
+        lblExercise2.TabIndex = 10
+        lblExercise2.Text = "Output all of the items in the same message box on seperate lines"
+        ' 
+        ' lblExercise3
+        ' 
+        lblExercise3.AutoSize = True
+        lblExercise3.Location = New Point(115, 112)
+        lblExercise3.Name = "lblExercise3"
+        lblExercise3.Size = New Size(334, 15)
+        lblExercise3.TabIndex = 11
+        lblExercise3.Text = "Add up all of the items then output the total in a message box"
+        ' 
+        ' lblExercise4
+        ' 
+        lblExercise4.AutoSize = True
+        lblExercise4.Location = New Point(115, 141)
+        lblExercise4.Name = "lblExercise4"
+        lblExercise4.Size = New Size(356, 15)
+        lblExercise4.TabIndex = 12
+        lblExercise4.Text = "Calculate the average of the items then output it in a message box"
+        ' 
+        ' lblExercise5
+        ' 
+        lblExercise5.AutoSize = True
+        lblExercise5.Location = New Point(115, 170)
+        lblExercise5.Name = "lblExercise5"
+        lblExercise5.Size = New Size(392, 15)
+        lblExercise5.TabIndex = 13
+        lblExercise5.Text = "Add up the items greater than 20 then output the result in a message box"
+        ' 
+        ' lblExercise6
+        ' 
+        lblExercise6.AutoSize = True
+        lblExercise6.Location = New Point(115, 199)
+        lblExercise6.Name = "lblExercise6"
+        lblExercise6.Size = New Size(285, 15)
+        lblExercise6.TabIndex = 14
+        lblExercise6.Text = "Find the largest item then output it in a message box"
+        ' 
+        ' lblExercise7
+        ' 
+        lblExercise7.AutoSize = True
+        lblExercise7.Location = New Point(115, 228)
+        lblExercise7.Name = "lblExercise7"
+        lblExercise7.Size = New Size(289, 15)
+        lblExercise7.TabIndex = 15
+        lblExercise7.Text = "Find the smallest item and output it in a message box"
+        ' 
+        ' lblExercise8
+        ' 
+        lblExercise8.AutoSize = True
+        lblExercise8.Location = New Point(115, 257)
+        lblExercise8.Name = "lblExercise8"
+        lblExercise8.Size = New Size(503, 15)
+        lblExercise8.TabIndex = 16
+        lblExercise8.Text = "Replace each item in the array with a new value that is twice as big then output the new values"
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(652, 327)
+        Controls.Add(lblExercise8)
+        Controls.Add(lblExercise7)
+        Controls.Add(lblExercise6)
+        Controls.Add(lblExercise5)
+        Controls.Add(lblExercise4)
+        Controls.Add(lblExercise3)
+        Controls.Add(lblExercise2)
+        Controls.Add(lblExercise1)
+        Controls.Add(btnExercise8)
+        Controls.Add(btnExercise7)
+        Controls.Add(btnExercise6)
+        Controls.Add(btnExercise5)
+        Controls.Add(btnExercise4)
+        Controls.Add(btnExercise3)
+        Controls.Add(btnExercise2)
+        Controls.Add(btnExercise1)
+        Controls.Add(lblCodeTitle)
+        Name = "form1"
+        Text = "Coding Exercise"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblCodeTitle As Label
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents btnExercise2 As Button
+    Friend WithEvents btnExercise3 As Button
+    Friend WithEvents btnExercise4 As Button
+    Friend WithEvents btnExercise5 As Button
+    Friend WithEvents btnExercise6 As Button
+    Friend WithEvents btnExercise7 As Button
+    Friend WithEvents btnExercise8 As Button
+    Friend WithEvents lblExercise1 As Label
+    Friend WithEvents lblExercise2 As Label
+    Friend WithEvents lblExercise3 As Label
+    Friend WithEvents lblExercise4 As Label
+    Friend WithEvents lblExercise5 As Label
+    Friend WithEvents lblExercise6 As Label
+    Friend WithEvents lblExercise7 As Label
+    Friend WithEvents lblExercise8 As Label
+End Class
